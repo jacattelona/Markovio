dc7c930602bb19e32161777fcc28dae6ac6d35cc
Unknown <jacattelona@wpi.edu>
2019-11-02 14:46:26 -0400

    Package and Generator

---
+++ Mario-AI-Framework-master/src/levelGenerators/CattelonaCampbellGenerator/LevelGenerator.java
@@ -0,0 +1,19 @@
+package levelGenerators.CattelonaCampbellGenerator;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import engine.core.MarioLevelGenerator;
+import engine.core.MarioLevelModel;
+import engine.core.MarioTimer;
+
+public class LevelGenerator implements MarioLevelGenerator{
+
+    public String getGeneratedLevel(MarioLevelModel model, MarioTimer timer){
+        return "";
+    }
+
+    public String getGeneratorName(){
+        return "CattelonaCampbellGenerator";
+    }
+}

c9a0c5c9f1be476c6d7fc962f1a5d94f8cc6f1fa
Unknown <jacattelona@wpi.edu>
2019-12-04 12:05:24 -0500

    Tasks

---
+++ Mario-AI-Framework-master/src/agents/CattelonaCampbell/Tasks.java
@@ -0,0 +1,11 @@
+package agents.CattelonaCampbell;
+
+import engine.core.MarioForwardModel;
+import engine.core.MarioTimer;
+
+public class Tasks {
+
+    public static boolean HopOnEnemy(MarioForwardModel model, MarioTimer timer){
+
+    }
+}

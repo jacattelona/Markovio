ca20a7f94172ca5de79fc621abb42771c365b0b2
Unknown <jacattelona@wpi.edu>
2019-12-04 12:03:16 -0500

    Basics

--- Mario-AI-Framework-master/src/agents/CattelonaCampbell/BehaviorTree.java
+++ Mario-AI-Framework-master/src/agents/CattelonaCampbell/BehaviorTree.java
@@ -4,11 +4,61 @@ import engine.core.MarioForwardModel;
 import engine.core.MarioTimer;
 
 public class BehaviorTree {
+    boolean once = true;
 
     public BehaviorTree(){
     }
 
     public boolean[] chooseActions(MarioForwardModel model, MarioTimer timer){
-        return null;
+        /*
+        if (once){
+            int go[][] = model.getScreenCompleteObservation(0, 0);
+            for (int y = 0; y < go.length; y++){
+                for (int x = 0; x < go[y].length; x++){
+                    System.out.print(go[x][y]);
+                    if (go[x][y] < 9)
+                        System.out.print("  ");
+                    else
+                        System.out.print(" ");
+                }
+                System.out.println();
+            }
+            System.out.println("------------");
+
+            int go2[][] = model.getMarioCompleteObservation(0, 0);
+            for (int y = 0; y < go2.length; y++){
+                for (int x = 0; x < go2[y].length; x++){
+                    System.out.print(go2[x][y]);
+                    if (go2[x][y] < 9)
+                        System.out.print("  ");
+                    else
+                        System.out.print(" ");
+                }
+                System.out.println();
+            }
+            System.out.println("------------");
+            once = false;
+        }
+        */
+
+        boolean actions[] = new boolean[]{false, true, false, true, false};
+        int info[][] = model.getScreenCompleteObservation(0, 0);
+        int pos[] = model.getMarioScreenTilePos();
+
+        if (info[pos[0]+1][pos[1]] != 0){
+            actions[4] = true;
+            actions[1] = false;
+            actions[0] = true;
+            System.out.println("Test");
+        }
+
+        if (info[pos[0]+1][pos[1]+1] == 0) actions[4] = true;
+
+        int enemies[][] = model.getScreenEnemiesObservation(0);
+        if (info[pos[0]+1][pos[1]] != 0 || info[pos[0]+2][pos[1]]!= 0)
+            actions[4] = true;
+
+        if (!model.isMarioOnGround()) actions[4] = true;
+        return actions;
     }
 }

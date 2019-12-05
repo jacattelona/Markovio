68e0a59e71326862bc8b83b3af1d4de5cf2d168b
Unknown <jacattelona@wpi.edu>
2019-12-05 09:58:15 -0500

    Balanced Tree

--- Mario-AI-Framework-master/src/agents/CattelonaCampbell/BehaviorTree.java
+++ Mario-AI-Framework-master/src/agents/CattelonaCampbell/BehaviorTree.java
@@ -14,27 +14,36 @@ public class BehaviorTree {
 
     public boolean[] chooseActions(MarioForwardModel model, MarioTimer timer){
 
+        //default walk right
         boolean actions[] = new boolean[]{false, true, false, false, false};
         int info[][] = model.getScreenCompleteObservation(0, 0);
         int pos[] = model.getMarioScreenTilePos();
 
-        if (tasks.ObstacleAhead(model)){
-            actions = tasks.JumpOnObstacle(model, timer);
-        }
-        // else if gap jump across
-        else if ((info[pos[0] + 1][15] != 17 || info[pos[0] + 2][15] != 17)
-                && info[pos[0] + 1][pos[1] + 1] == 0){
-            actions[4] = true;
+        //Obstacle or Gap
+        if (tasks.ObstacleAhead(model) || ((info[pos[0] + 1][15] != 17 || info[pos[0] + 2][15] != 17))){
+
+            if (tasks.ObstacleAhead(model))
+                actions = tasks.JumpOnObstacle(model, timer);
+
+            // else if gap jump across
+            else if ((info[pos[0] + 1][15] != 17 || info[pos[0] + 2][15] != 17)
+                    && info[pos[0] + 1][pos[1] + 1] == 0){
+                actions[4] = true;
+            }
         }
 
-        //if enemy, go kill it
-        else if (!Arrays.equals(new int[]{-1, -1}, tasks.findClosestEnemy(model, 3))) {
-            actions = tasks.HopOnEnemy(model, timer, 3);
-        }
 
-        // TODO: else if power up on screen, catch it
-        else if (!Arrays.equals(new int[]{-1, -1}, tasks.findClosestPowerup(model, 8))) {
-            actions = tasks.GetPowerup(model, timer, 8);
+
+        //Enemy or Powerup
+        else if (!Arrays.equals(new int[]{-1, -1}, tasks.findClosestEnemy(model, 3)) ||
+                (!Arrays.equals(new int[]{-1, -1}, tasks.findClosestPowerup(model, 8)))) {
+
+            if (!Arrays.equals(new int[]{-1, -1}, tasks.findClosestEnemy(model, 3)))
+                actions = tasks.HopOnEnemy(model, timer, 3);
+
+            else if (!Arrays.equals(new int[]{-1, -1}, tasks.findClosestPowerup(model, 8))) {
+                actions = tasks.GetPowerup(model, timer, 8);
+            }
         }
 
 
@@ -43,6 +52,8 @@ public class BehaviorTree {
             actions = tasks.HitQuestionBlock(model, timer);
         }
 
+
+        //reset jump flat
         if (jumpFlag && model.isMarioOnGround()){
             jumpFlag = false;
             actions[4] = false;

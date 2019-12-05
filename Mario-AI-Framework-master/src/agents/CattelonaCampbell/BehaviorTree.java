b3936383ba34c539586c6a156f6513625cb2420b
Unknown <jacattelona@wpi.edu>
2019-12-05 01:32:49 -0500

    I̷̮͐'̷̺̰̹̑̏m̸̡̨̭̪͔͎̭̚͜ ̴̠̈̎̏S̷͙̗̈́̄̃̌̈́̕͝ù̵̦͇̭̻̰̩̅͗̉̊͜f̷̖̳̲̩̪̹̩̞̿̈́f̴̩̪̰͔͔͎̄̓͝e̸̡͎̭̘̺̦̥͓̬̐r̵̥̺͚̼̟̬̖̔͑̅̓͠ḯ̵͍̞̞͖͉͙̯̥͜ͅn̷̢̪͚̾̍̓g̵̺̟̠̈͋͗

--- Mario-AI-Framework-master/src/agents/CattelonaCampbell/BehaviorTree.java
+++ Mario-AI-Framework-master/src/agents/CattelonaCampbell/BehaviorTree.java
@@ -22,15 +22,21 @@ public class BehaviorTree {
             actions = tasks.JumpOnObstacle(model, timer);
         }
         // else if gap jump across
-        else if (info[pos[0] + 1][15] != 17
+        else if ((info[pos[0] + 1][15] != 17 || info[pos[0] + 2][15] != 17)
                 && info[pos[0] + 1][pos[1] + 1] == 0){
             actions[4] = true;
         }
+
+        //if enemy, go kill it
         else if (!Arrays.equals(new int[]{-1, -1}, tasks.findClosestEnemy(model, 3))) {
             actions = tasks.HopOnEnemy(model, timer, 3);
         }
+
         // TODO: else if power up on screen, catch it
-        
+        else if (!Arrays.equals(new int[]{-1, -1}, tasks.findClosestPowerup(model, 8))) {
+            actions = tasks.GetPowerup(model, timer, 8);
+        }
+
 
         // else if question block punch it
         else if (tasks.HitQuestionBlock(model, timer) != null){
--- Mario-AI-Framework-master/src/agents/CattelonaCampbell/Tasks.java
+++ Mario-AI-Framework-master/src/agents/CattelonaCampbell/Tasks.java
@@ -4,6 +4,7 @@ import engine.core.MarioForwardModel;
 import engine.core.MarioTimer;
 
 public class Tasks {
+    private int obstacleEdge = 0;
 
     public Tasks() {
     }
@@ -17,7 +18,7 @@ public class Tasks {
         // check space above Mario for ? Block
         boolean q_block = false;
         for (int i = 1; i < 4; i++){
-            if (pos[1] - i > 0 && pos[1] - i < info.length) {
+            if (pos[1] - i > 0 && pos[1] - i < info.length && pos[0]-1 >= 0 && pos[0]+1 <= info.length) {
                 if (info[pos[0] - 1][pos[1] - i] == 24) {
                     q_block = true;
                     actions[0] = true;
@@ -51,13 +52,13 @@ public class Tasks {
     public int[] findClosestEnemy(MarioForwardModel model, int radius){
         int closestEnemy[] = new int[]{-1, -1};
         double closestDistance = 1000;
-        int enemyInfo[][] = model.getScreenEnemiesObservation(0);
+        int enemyInfo[][] = model.getScreenEnemiesObservation(1);
         int pos[] = model.getMarioScreenTilePos();
 
-        for (int y = pos[1] - 1; y < pos[1] + radius; y++){
+        for (int y = pos[1]; y < pos[1] + radius; y++){
             for (int x = pos[0] - radius + 2; x < pos[1] + radius; x++){
                 if ((y >= 0 && x >= 0) && (y < enemyInfo[0].length && x < enemyInfo.length)){
-                    if (2 <= enemyInfo[x][y] && enemyInfo[x][y] <= 7){
+                    if (enemyInfo[x][y] == MarioForwardModel.OBS_STOMPABLE_ENEMY || enemyInfo[x][y] == MarioForwardModel.OBS_NONSTOMPABLE_ENEMY){
                         int enemyPos[] = new int[]{x, y};
                         double newDistance = getDistance(pos, enemyPos);
                         if (newDistance < closestDistance){
@@ -72,18 +73,100 @@ public class Tasks {
         return closestEnemy;
     }
 
-    public boolean[] HopOnEnemy(MarioForwardModel model, MarioTimer timer, int radius){
+    public int[] findClosestPowerup(MarioForwardModel model, int radius){
+        int closestPowerup[] = new int[]{-1, -1};
+        double closestDistance = 1000;
+        int enemyInfo[][] = model.getScreenEnemiesObservation(0);
+        int pos[] = model.getMarioScreenTilePos();
+
+        for (int y = pos[1] - 1; y < pos[1] + radius; y++){
+            for (int x = pos[0] - radius; x < pos[1] + radius; x++){
+                if ((y >= 0 && x >= 0) && (y < enemyInfo[0].length && x < enemyInfo.length)){
+                    if (enemyInfo[x][y] == MarioForwardModel.OBS_MUSHROOM){
+                        int powerupPos[] = new int[]{x, y};
+                        double newDistance = getDistance(pos, powerupPos);
+                        if (newDistance < closestDistance){
+                            closestDistance = newDistance;
+                            closestPowerup = powerupPos;
+                        }
+                    }
+                }
+
+            }
+        }
+
+        return closestPowerup;
+    }
+
+    public boolean[] GetPowerup(MarioForwardModel model, MarioTimer timer, int radius){
         boolean actions[] = new boolean[]{false, false, false, false, false};
 
         int pos[] = model.getMarioScreenTilePos();
 
-        int closestEnemy[] = findClosestEnemy(model, radius);
+        int closestPowerup[] = findClosestPowerup(model, radius);
 
-        if (closestEnemy[0] > pos[0]) actions[1] = true;
-        if (closestEnemy[0] <= pos[0]){
+        if (closestPowerup[0] > pos[0]) actions[1] = true;
+        if (closestPowerup[0] <= pos[0]){
             actions[0] = true;
             //actions[3] = true;
         }
+
+
+        if (Math.abs(closestPowerup[0] - pos[0]) < radius && (pos[1] > closestPowerup[1]-1)){
+            actions[4] = true;
+        }
+
+        return actions;
+    }
+
+    public boolean[] HopOnEnemy(MarioForwardModel model, MarioTimer timer, int radius){
+        boolean actions[] = new boolean[]{false, false, false, false, false};
+        int enemyInfo[][] = model.getScreenEnemiesObservation(1);
+        int pos[] = model.getMarioScreenTilePos();
+        int closestEnemy[] = findClosestEnemy(model, radius);
+
+        //if enemy to the right, move right
+        if (closestEnemy[0] > pos[0]){
+            actions[1] = true;
+
+            /*
+            if (closestEnemy[1] == pos[1]){
+                int enemyDetail[][] = model.getScreenEnemiesObservation(0);
+                if (enemyDetail[closestEnemy[0]][closestEnemy[1]] == 8){
+                    System.out.println("die");
+                    actions[1] = false;
+                    return actions;
+                }
+
+            }
+            if (closestEnemy[1] == pos[1]+1){
+                actions[4] = true;
+            }
+            else{
+                actions[1] = true;
+            }
+            */
+
+        }
+
+        //if enemy to the left, move left, unless unstompable, then move right
+        if (closestEnemy[0] <= pos[0]){
+
+            //if in array bounds
+            if (closestEnemy[0] >= 0 && closestEnemy[0] < enemyInfo.length && closestEnemy[1] >= 0 && closestEnemy[1] < enemyInfo.length){
+
+                //if stompable, move left
+                if (enemyInfo[closestEnemy[0]][closestEnemy[1]] == MarioForwardModel.OBS_STOMPABLE_ENEMY){
+                    actions[0] = true;
+                }
+
+                //otherwise, move right
+                else{
+                    actions[1] = true;
+                }
+            }
+        }
+
 
 
         if (Math.abs(closestEnemy[0] - pos[0]) < radius && (pos[1] > closestEnemy[1]-1)){
@@ -93,16 +176,6 @@ public class Tasks {
         return actions;
     }
 
-    public boolean[] HopOverObstacle(MarioForwardModel model, MarioTimer timer){
-        boolean actions[] = new boolean[]{false, false, false, false, false};
-        // get the info of what's on screen
-        int info[][] = model.getScreenSceneObservation(0);
-        // get Mario's location on screen
-        int pos[] = model.getMarioScreenTilePos();
-
-
-        return actions;
-    }
 
     public boolean ObstacleAhead(MarioForwardModel model){
         int info[][] = model.getScreenSceneObservation(1);
@@ -110,7 +183,7 @@ public class Tasks {
 
         for (int y = pos[1]; y >= pos[1] - 2; y--){
             for (int x = pos[0]; x <= pos[0] + 3; x++){
-                if (y >= 0 && y < model.obsGridHeight){
+                if (y >= 0 && y < model.obsGridHeight && x >= 0 && x < model.obsGridWidth){
                     //int blockVal = MarioForwardModel.getBlockValueGeneralization(info[x][y], 1);
                     int blockVal = info[x][y];
                     if (blockVal == MarioForwardModel.OBS_SOLID || blockVal == MarioForwardModel.OBS_CANNON || blockVal == MarioForwardModel.OBS_PIPE){
@@ -144,6 +217,19 @@ public class Tasks {
 
         if (!above)
             actions[1] = true;
+        /*
+        if (above){
+            obstacleEdge++;
+            if (obstacleEdge % 3 == 0){
+                obstacleEdge = 0;
+                actions[0] = true;
+            }
+            else{
+                actions[0] = false;
+            }
+        }
+        */
+
         return actions;
     }
 }
